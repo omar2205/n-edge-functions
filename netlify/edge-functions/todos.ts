b713235cf4c1e742b18971782c4b13308c247051
omar2205 <omar2205a@yahoo.com>
2022-04-20 07:48:43 +0000

    test post method

--- netlify/edge-functions/todos.ts
+++ netlify/edge-functions/todos.ts
@@ -9,14 +9,6 @@ export default async (req: Request, ctx: any) => {
       status: 500,
     })
 
-  switch (req.method) {
-    case 'POST':
-      const id = `todo_${nanoid(4)}`
-      const {title, done} = await req.json()
-      if (title && done) await redis.hset(id, { title, done })
-      break
-  }
-
   const todosKeys = await redis.keys('todo_*')
   const todos = await Promise.all(
     todosKeys.map(async (k: string) => getObj(await redis.hgetall(k)))
